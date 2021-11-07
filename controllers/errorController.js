2488f005dfcd7c992322d0f2b4f9b0a269621f61
Taylor Laing <62392492+taylorlaing8@users.noreply.github.com>
2021-11-07 13:57:58 -0700

    Revert "Updated the project milestone 3 journal"

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const httpStatus = require('http-status-codes');
+const httpStatus = require("http-status-codes");
 
 exports.logErrors = (error, req, res, next) => {
-	console.error(error.stack);
-	next(error);
+  console.error(error.stack);
+  next(error);
 };
 
 exports.respondNoResourceFound = (req, res) => {
-	let errorCode = httpStatus.NOT_FOUND;
-	res.status(errorCode);
-	res.send(`${errorCode} | The page does not exist!`);
+  let errorCode = httpStatus.NOT_FOUND;
+  res.status(errorCode);
+  res.send(`${errorCode} | The page does not exist!`);
 };
 
 exports.respondInternalError = (error, req, res, next) => {
-	let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
-	console.log(`ERROR occurred: ${error.stack}`);
-	res.status(errorCode);
-	res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+  let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+  console.log(`ERROR occurred: ${error.stack}`);
+  res.status(errorCode);
+  res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
 };
