ae5d6630b27f3ff148a2c0c01adf65415bae0590
Taylor Laing <taylorlaing8@gmail.com>
2021-11-29 19:46:10 -0700

    Update shop for dynmic content

--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -2,9 +2,11 @@
 
 const router = require("express").Router(),
     shopController = require("../controllers/shopController"),
-    categoryController = require("../controllers/categoryController");
+    categoryController = require("../controllers/categoryController"),
+    productController = require("../controllers/productController");
 
-router.get("/", shopController.index, shopController.indexView);
+// router.get("/", shopController.index, shopController.indexView);
+router.get("/", categoryController.getAll, productController.getAll, shopController.indexView);
 router.get("/:slug", categoryController.index, categoryController.indexView);
 
 module.exports = router;
