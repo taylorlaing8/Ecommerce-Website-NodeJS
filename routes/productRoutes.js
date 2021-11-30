31455f26ebb8f459844618ab6a36834a6be2a3c2
Taylor Laing <taylorlaing8@gmail.com>
2021-11-29 22:49:25 -0700

    product page update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const router = require("express").Router(),
-    productController = require("../controllers/productController");
+    productController = require("../controllers/productController"),
+    variationController = require("../controllers/variationController");
 
 router.post("/update", productController.update, productController.redirectView);
 router.post("/create", productController.create, productController.redirectView);
-router.get("/:slug", productController.index, productController.indexView);
+router.get("/:slug", productController.index, variationController.getAllVars, variationController.getAllVarTypes, productController.indexView);
 router.post("/:slug/add-image", productController.index, productController.addImage, productController.redirectView);
 router.post("/:slug/remove", productController.remove, productController.redirectView);
 router.get("/:slug/image/:imgId/remove", productController.removeImage, productController.redirectView);
